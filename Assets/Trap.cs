5eebf4f4b97c0f729f81e7fbfbe5e4e0348d6e57
Tan Liyuan <tanliyuan1378@gmail.com>
2022-08-22 19:10:04 +1000

    trap

---
+++ Assets/Trap.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public float time;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Trapping(other.transform));
+        print(other);
+    }
+
+    IEnumerator Trapping(Transform target)
+    {
+        float time = this.time;
+        while (time > 0)
+        {
+            if(target.position != transform.position)
+            {
+                Vector3 position = Vector3.MoveTowards(target.position, transform.position, Time.deltaTime*10);
+                position.y = target.position.y;
+                target.position=position;
+            }
+            time -= Time.deltaTime;
+            yield return null;
+        }
+    }
+}
