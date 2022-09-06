ccd32c9b75ba4162d0f38b8dba6397be8007a4b1
Tan Liyuan <tanliyuan1378@gmail.com>
2022-09-06 18:10:30 +1000

    The navigation system was introduced, and butterflies were created.

---
+++ Assets/Butterfly.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butterfly : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent agent;
+    public GameObject target;
+
+    void Start()
+    {
+        //获取角色上的NavMeshAgent组件
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        InvokeRepeating(nameof(UpdateDestination), 0, 3);
+    }
+
+    private void Update()
+    {
+        //鼠标左键
+        if (Input.GetMouseButtonDown(0))
+        {
+            //射线检测
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool isCollider = Physics.Raycast(ray, out hit);
+            if (isCollider)
+            {
+                //hit.point射线触碰的Position
+                //SetDestination设置下一步的位置
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+
+    void UpdateDestination()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
