0231d3ca17f6cb6b25a0d22c77aaeb91461b233c
Rigyuki <304900272@qq.com>
2023-04-04 12:16:34 +0800

    test ghost book

---
+++ Assets/Scripts/Gameplay/GhostBook/StateManager.cs
@@ -0,0 +1,66 @@
+using Scripts.CustomTool.DesignPatterns.ObserverPattern;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.Gameplay.GhostBook
+{
+    // TODO: find object with correct name...change sprite to correct file name 
+    public class StateManager : MonoBehaviour
+    {
+        private Image img_Book;
+        private Image img_Taichi;
+
+        private Button btn_book;
+        private Button btn_Taichi;
+        private void Start()
+        {
+            img_Book = GameObject.Find("Canvas/GhostBook/book").GetComponent<Image>();
+            img_Taichi = GameObject.Find("Canvas/Taichi").GetComponent<Image>();
+
+            btn_book = GameObject.Find("btn_Start").GetComponent<Button>();
+            btn_book.onClick.AddListener(OnStartBtnBookClick);
+
+            btn_Taichi = GameObject.Find("btn_Start").GetComponent<Button>();
+            btn_Taichi.onClick.AddListener(OnStartBtnTaichiClick);
+        }
+        private void OnEnable()
+        {
+            MsgCenterByList.AddListener(OnMsg);
+        }
+
+        private void OnDisable()
+        {
+            MsgCenterByList.RemoveListener(OnMsg);
+        }
+
+        private void OnMsg(CommonMsg obj)
+        {
+            if (obj.MsgId == MsgCenterByList.COLLECTION_GHOST || obj.MsgId== MsgCenterByList.COLLECTION_LU)
+            {
+                img_Book.sprite = Resources.Load("GhostBook/UnreadBook", typeof(Sprite)) as Sprite;
+            }
+
+            // TODO: change taichi image
+            if (obj.MsgId == MsgCenterByList.COLLECTION_GHOST)
+            {
+                img_Taichi.sprite = Resources.Load("GhostBook/UnreadTaichi", typeof(Sprite)) as Sprite;
+            }
+        }
+
+       private void OnStartBtnBookClick()
+        {
+            img_Book.sprite = Resources.Load("GhostBook/NormalBook", typeof(Sprite)) as Sprite;
+        }
+
+        private void OnStartBtnTaichiClick()
+        {
+            img_Taichi.sprite = Resources.Load("GhostBook/NormalTaichi", typeof(Sprite)) as Sprite;
+
+        }
+
+    }
+
+}
+
--- Assets/Scripts/Gameplay/GhostBook/SwitchBook.cs
+++ Assets/Scripts/Gameplay/GhostBook/SwitchBook.cs
@@ -14,31 +14,44 @@ namespace Scripts.Gameplay.GhostBook {
         private Button anecdoteButton;
         private Button collectionButton;
 
+        private Button exitButton;
+        private Button openBookButton;
 
         public int currentAmount = 0;
 
         private Transform anecdote;
         private Transform collection;
 
+        [SerializeField]private Transform GhostBook;
 
+
+         
         private void Start()
         {
+          
+            // GhostBook.gameObject.SetActive(false);
+
             leftButton = GameObject.Find("Canvas/GhostBook/btn_Left").GetComponent<Button>();
             rightButton = GameObject.Find("Canvas/GhostBook/btn_Right").GetComponent<Button>();
 
             anecdoteButton = GameObject.Find("Canvas/GhostBook/btn_Anecdote").GetComponent<Button>();
             collectionButton = GameObject.Find("Canvas/GhostBook/btn_Collection").GetComponent<Button>();
 
+            exitButton=GameObject.Find("Canvas/GhostBook/btn_Exit").GetComponent<Button>();
+            openBookButton = GameObject.Find("Canvas/btn_OpenBook").GetComponent<Button>();
 
             leftButton.onClick.AddListener(delegate { switchAnecdotePage(-1); });
             rightButton.onClick.AddListener(delegate { switchAnecdotePage(1); });
             anecdoteButton.onClick.AddListener(delegate { switchBookItemImage(true); });
             collectionButton.onClick.AddListener(delegate { switchBookItemImage(false); });
 
+            exitButton.onClick.AddListener(delegate { CloseBook(); });
+            openBookButton.onClick.AddListener(delegate { OpenBook(); });
+
             anecdote = GameObject.Find("Canvas/GhostBook/Anecdote").transform;
             collection = GameObject.Find("Canvas/GhostBook/Collection").transform;
 
-
+            //GhostBook = GameObject.Find("Canvas/GhostBook").transform;
         }
 
         public void switchAnecdotePage(int amount)
@@ -65,6 +78,21 @@ namespace Scripts.Gameplay.GhostBook {
                 anecdoteButton.enabled = true;
                 collectionButton.enabled = false;
             }
+        }
+
+        /* private void OpenOrCloseBook(bool status)
+         {
+             GhostBook.gameObject.SetActive(status);
+         }*/
+
+        private void CloseBook()
+        {
+            GhostBook.gameObject.SetActive(false);
+        }
+
+        private void OpenBook()
+        {
+            GhostBook.gameObject.SetActive(true);
         }
 
     }
